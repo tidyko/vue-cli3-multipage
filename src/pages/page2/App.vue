0ecabf2ba07ac4bf53a2ae37059670f24c7ee0d9
曾伟明 <851008842@qq.com>
2018-11-07 16:06:51 +0800

    no message

--- src/pages/page2/App.vue
+++ src/pages/page2/App.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="app">
-    <a href="/">index</a> | <a href="/page1.html">page1</a> | <a href="/page2.html">page2</a> 
+    <a href="/">index</a> | <a href="/page1.html">page1</a> | <a href="/page2.html">page2</a>
     <h2>this is page2 index page</h2>
+    <a @click="test1">test1</a>
   </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
+//axios
+import Vue from 'vue'
+Vue.prototype.$http = api
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },methods:{
+    test1: function(){
+      this.$http.get({
+      type: 'json', baseUrl: 'http://localhost:8080/api', api: '/demo'},
+      null,
+      response=>{
+        console.log("response")
+        console.log(response)
+      },
+      error=>{
+        console.log("error")
+        console.log(error[0])
+      });
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
